4afe36203b7abbb73ac9cdacb5311e96fbed6033
pontus393 <pontus.loefgren@gmail.com>
2022-12-21 21:16:33 +0100

    Finish 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+
+    let outputString = ""
+
+    if (num < 0) {
+        return "ERROR";
+    } else {
+        for (let i = 0; i < num; i++) {
+            outputString += string;
+        }
+        return outputString;
+    }
 
 };
 
