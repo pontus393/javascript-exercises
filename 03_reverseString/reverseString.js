37601c3ce178d909127a4b1a52399c7ea0b4f201
pontus393 <pontus.loefgren@gmail.com>
2022-12-21 21:38:10 +0100

    Finish 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    let stringChars = string.split("");
+    let reversedString = "";
+
+    for (let i = 1; i <= stringChars.length; i++) {
+        reversedString += stringChars[stringChars.length - i];
+    }
+
+    return reversedString;
 };
+
+console.log(reverseString("hello world"));
 
 // Do not edit below this line
 module.exports = reverseString;
