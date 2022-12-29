63ddc6b20085564387741e8d78f970cb5ad9342f
pontus393 <pontus.loefgren@gmail.com>
2022-12-29 18:35:50 +0100

    Finish exercice 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+
+  celcius = Math.round(((fahrenheit - 32) * 5/9) * 10) / 10; 
+  return celcius;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+
+  fahrenheit = Math.round(((celsius * 9/5 + 32) * 10)) / 10; 
+  return fahrenheit;
+
 };
 
 // Do not edit below this line
