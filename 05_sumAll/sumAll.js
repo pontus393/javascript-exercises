b7b3f680a01593a0223bc6aef494eb75ddc69ed5
pontus393 <pontus.loefgren@gmail.com>
2022-12-29 18:06:13 +0100

    Finish exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function(firstInt, secondInt) {
+
+    let result = 0;
+    let start = Math.min(firstInt, secondInt);
+    let end = Math.max(firstInt, secondInt);
+
+    if (firstInt < 0 || secondInt < 0 || typeof(firstInt) != "number" || typeof(secondInt) != "number") {
+        return "ERROR";
+    } else {
+
+        for (let i = start; i <= end; i++) {
+            result += i;
+        }
+
+
+    }
+
+    return result;
 
 };
 
