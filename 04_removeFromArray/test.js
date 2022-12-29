027b436b3d2ed4901ae8a440a7794e218007a666
pontus393 <pontus.loefgren@gmail.com>
2022-12-29 13:02:54 +0100

    Remove test.js

--- 04_removeFromArray/test.js
+++
@@ -1,5 +0,0 @@
-const cats = ['Leopard', 'Serval', 'Jaguar', 'Tiger', 'Caracal', 'Lion'];
-
-const filtered = cats.filter((cat) => cat.startsWith('L'));
-console.log(filtered);
-// [ "Leopard", "Lion" ]
