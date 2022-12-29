9eec2d7979b4e96e50c2e5d6c371e004a2c0bf0d
pontus393 <pontus.loefgren@gmail.com>
2022-12-29 21:15:04 +0100

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,36 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+
+    if (n < 0) {
+        return "OOPS";
+    }
+
+    fibNumbers = [0,1];
+
+    while (true) {
+
+        if (fibNumbers.length <= n) {
+            
+            innerCount = 0;
+            
+            while (true) {
+
+                if (fibNumbers[fibNumbers.length -2] + fibNumbers[fibNumbers.length -1] == innerCount) {
+                    fibNumbers.push(innerCount);
+                    break;
+                } else {
+                    innerCount++;
+                    continue;
+                }
+
+            }
+
+        } else {
+            
+            return fibNumbers[fibNumbers.length - 1];
+
+            break;
+        }
+    }
 
 };
 
