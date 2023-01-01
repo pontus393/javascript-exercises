12844e861c747f0ce9a62b1493813a0d9503e253
pontus393 <pontus.loefgren@gmail.com>
2023-01-01 17:12:50 +0100

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    
+    let ages = [];
 
+    for (let x of Object.keys(arr)) {
+
+        if (arr[x].hasOwnProperty('yearOfDeath')) {
+
+            ages.push(arr[x].yearOfDeath - arr[x].yearOfBirth);
+
+        } else {
+
+            let date = new Date()
+            ages.push(date.getFullYear() - arr[x].yearOfBirth);
+        }
+    }
+
+    return arr[ages.indexOf(Math.max(...ages))]
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
