b9d5c2b0949b6c446b2fba18f5ab44f90757d698
pontus393 <pontus.loefgren@gmail.com>
2023-06-26 22:04:22 +0200

    Improve ex. 12 findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,22 +1,16 @@
 const findTheOldest = function(arr) {
-    
-    let ages = [];
 
-    for (let x of Object.keys(arr)) {
-
-        if (arr[x].hasOwnProperty('yearOfDeath')) {
-
-            ages.push(arr[x].yearOfDeath - arr[x].yearOfBirth);
-
+    let ages = arr.map(person => {
+        if (person.hasOwnProperty('yearOfDeath')) {
+            return person.yearOfDeath - person.yearOfBirth;
         } else {
-
-            let date = new Date()
-            ages.push(date.getFullYear() - arr[x].yearOfBirth);
+            return new Date().getFullYear() - person.yearOfBirth;
         }
-    }
+    });
 
     return arr[ages.indexOf(Math.max(...ages))]
-};
+
+}
 
 
 // Do not edit below this line
